b52f984c22bfb889afc0c708bc4b74263dd879ee
SARR-byte <travis.hale@ServiceProsHub.com>
2025-05-14 08:00:27 -0500

    Fix app preview configuration

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,16 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    hmr: {
+      clientPort: 443,
+    },
+    headers: {
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+    },
+  },
+  preview: {
+    port: 5173,
+    host: true,
   },
 });
